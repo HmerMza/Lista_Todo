9bad90675d6003d97bdd09b9dc564b7038b9629e
HmerMza <hkooze7@gmail.com>
2023-07-23 04:18:47 -0500

    Dando funcionalidad a los metodos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CompHeader from "./assets/components/CompHeader";
 import CompTodoCalculos from "./assets/components/CompTodoCalculos";
 import CompTodoForm from "./assets/components/CompTodoForm";
 import CompTodoList from "./assets/components/CompTodoList";
+
 const initialStateTodos = [
     { id: 1, title: "go to the gym", completed: true },
     {
@@ -16,12 +17,20 @@ const initialStateTodos = [
 ];
 const App = () => {
     const [todos, setTodos] = useState(initialStateTodos);
+    const createTodo = (title) => {
+        const newTodo = {
+            id: todos.length + 1,
+            title,
+            completed: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
     return (
         <div className="bg-[url(./assets/images/bg-mobile-light.jpg)] bg-contain   bg-no-repeat bg-gray-200 min-h-screen">
             <CompHeader />
 
             <main className="container mx-auto px-4 mt-8 ">
-                <CompTodoForm />
+                <CompTodoForm createTodo={createTodo} />
                 <CompTodoList todos={todos} />
                 <CompTodoCalculos />
                 <CompButtons />
--- src/assets/components/CompTodoForm.jsx
+++ src/assets/components/CompTodoForm.jsx
@@ -1,11 +1,27 @@
-const CompTodoForm = () => {
+import { useState } from "react";
+
+const CompTodoForm = ({ createTodo }) => {
+    const [title, setTitle] = useState("");
+    const handleSubmitAddTodo = (e) => {
+        e.preventDefault();
+        if (!title.trim()) {
+            return setTitle("");
+        }
+        createTodo(title);
+        setTitle("");
+    };
     return (
-        <form className="bg-white rounded-md overflow-hidden py-3 flex gap-4 items-center px-4 mt-8">
+        <form
+            onSubmit={handleSubmitAddTodo}
+            className="bg-white rounded-md overflow-hidden py-3 flex gap-4 items-center px-4 mt-8"
+        >
             <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
             <input
                 placeholder="create a new todo"
                 type="text"
                 className="w-full text-gray-400 outline-none"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
             />
         </form>
     );
